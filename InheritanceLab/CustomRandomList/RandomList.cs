ce6bdbb3cfb02acd2347b4a9d237ba55e14db229
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-01 09:54:15 +0200

    Add files via upload

---
+++ InheritanceLab/CustomRandomList/RandomList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public string RandomString()
+        {
+            var index = random.Next(0, Count);
+            return this[index];
+        }
+
+
+        public string RemoveRandomString()
+        {
+            int index = random.Next(0, Count);
+            string str = this[index];
+            this.RemoveAt(index);
+            return str;
+        }
+
+    }
+}
