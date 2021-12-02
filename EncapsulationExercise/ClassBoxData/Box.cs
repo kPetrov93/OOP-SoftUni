ad151781660f33afac9db066d317c91414aad65c
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-02 11:18:07 +0200

    Add files via upload

---
+++ EncapsulationExercise/ClassBoxData/Box.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+
+        private double width;
+
+        private double height;
+
+        public Box(double height, double width, double length)
+        {
+            Height = height;
+            Width = width;
+            Length = length;
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                height = value; 
+            }
+        }
+
+
+        public double Width
+        {
+            get { return width; }
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                width = value; 
+            }
+        }
+
+        public double Length 
+        {
+            get { return length; }
+            private set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                length = value; 
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            double result = (2 * length * width) + (2 * length * height) + (2 * width * height);
+            return result;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            double result = (2 * length * height) + (2 * width * height);
+            return result;
+        }
+
+        public double Volume()
+        {
+            double result =length*height*width;
+            return result;
+        }
+    }
+}
