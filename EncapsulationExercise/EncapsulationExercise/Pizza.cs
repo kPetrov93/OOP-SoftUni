96539accba5d6f0e61f950a61058338fe639a239
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-02 11:19:18 +0200

    Add files via upload

---
+++ EncapsulationExercise/EncapsulationExercise/Pizza.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EncapsulationExercise
+{
+    public class Pizza
+    {
+        private string name;
+        private Dough dough;
+        private List<Topping> toppings;
+
+        public Pizza(string name, Dough dough)
+        {
+            Name = name;
+            Dough = dough;
+
+            toppings = new List<Topping>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set 
+            {
+                if (value.Length<1 || value.Length>15)
+                {
+                    throw new ArgumentException("Pizza name should be between 1 and 15 symbols.");
+                }
+
+                name = value; 
+            }
+        }
+
+        public Dough Dough
+        {
+            get { return dough; }
+           private set { dough = value; }
+        }
+
+        public double TotalCalories()
+        {
+            double result = Dough.Calories + toppings.Sum(x => x.ToppingCalories);
+            return result;
+        }
+
+        public void AddToppings(Topping topping)
+        {
+            if (toppings.Count == 10)
+            {
+                throw new InvalidOperationException("Number of toppings should be in range [0..10].");
+            }
+
+            toppings.Add(topping);
+        }
+    }
+}
---
+++ EncapsulationExercise/EncapsulationExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EncapsulationExercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string[] pizzaInfo = Console.ReadLine().Split();
+                string[] doughInfo = Console.ReadLine().Split();
+                string input = Console.ReadLine();
+
+                string pizzaName = pizzaInfo[1];
+                string doughType = doughInfo[1];
+                string bakingTechnique = doughInfo[2];
+                double doughWeight = double.Parse(doughInfo[3]);
+
+                Dough dough = new Dough(doughType, bakingTechnique, doughWeight);
+                Pizza pizza = new Pizza(pizzaName, dough);
+
+                while (input != "END")
+                {
+                    string[] toppingInfo = input.Split();
+                    string toppingType = toppingInfo[1];
+                    double toppingWeight = double.Parse(toppingInfo[2]);
+
+                    Topping topping = new Topping(toppingType,toppingWeight);
+
+                    pizza.AddToppings(topping);
+
+                    input = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{pizza.Name} - {pizza.TotalCalories():f2} Calories.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
