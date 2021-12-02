3d8658b2cf77c9ceb8d37dc90417fed4840854b9
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-02 11:19:48 +0200

    Add files via upload

---
+++ EncapsulationExercise/EncapsulationExercise/Dough.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationExercise
+{
+    public class Dough
+    {
+        private string type;
+
+        private string technique;
+
+        private double weight;
+
+        Dictionary<string, double> flourType = new Dictionary<string, double>
+        { 
+            {"white" , 1.5},
+            {"wholegrain" , 1}
+        };
+
+        Dictionary<string, double> bakingTechnique = new Dictionary<string, double> 
+        {
+            {"crispy" , 0.9},
+            {"chewy" , 1.1},
+            {"homemade" , 1.0}
+        };
+
+        public Dough(string type, string technique, double weight)
+        {
+            Type = type;
+            Technique = technique;
+            Weight = weight;
+        }
+
+        public string Type
+        {
+            get { return type; }
+            private set 
+            {
+                if (!flourType.ContainsKey(value.ToLower()))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+
+                type = value; 
+            }
+        }
+
+        public string Technique
+        {
+            get { return technique; }
+            private set 
+            {
+                if (!bakingTechnique.ContainsKey(value.ToLower()))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+
+                technique = value; 
+            }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            private set 
+            {
+                if (value<1 || value>200)
+                {
+                    throw new ArgumentException("Dough weight should be in the range [1..200].");
+                }
+
+                weight = value; 
+            }
+        }
+
+        public double Calories => 2 * Weight * flourType[type.ToLower()] * bakingTechnique[technique.ToLower()];
+
+    }
+}
---
+++ EncapsulationExercise/EncapsulationExercise/Topping.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationExercise
+{
+    public class Topping
+    {
+        private Dictionary<string, double> toppingTypes = new Dictionary<string, double>
+        {
+            {"meat" , 1.2},
+            {"veggies" , 0.8},
+            {"sauce" , 0.9},
+            {"cheese" , 1.1}
+        };
+
+        private string type;
+
+        private double weight;
+
+        public Topping(string type, double weight)
+        {
+            Type = type;
+            Weight = weight;
+        }
+
+        public string Type
+        {
+            get { return type; }
+            private set
+            {
+                if (!toppingTypes.ContainsKey(value.ToLower()))
+                {
+                    throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+                }
+
+                type = value;
+            }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            private set
+            {
+                if (value < 1 || value > 50)
+                {
+                    throw new ArgumentException($"{Type} weight should be in the range [1..50].");
+                }
+
+                weight = value;
+            }
+        }
+
+        public double ToppingCalories => 2 * Weight * toppingTypes[Type.ToLower()];
+    }
+}
