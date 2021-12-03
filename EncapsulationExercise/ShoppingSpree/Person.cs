5eb2da018ffd9c19e8bf962127ed7a3961b75dbe
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-03 10:03:26 +0200

    Add files via upload

---
+++ EncapsulationExercise/ShoppingSpree/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private int money;
+        private List<Product> products;
+
+        public Person(string name, int money)
+        {
+            Name = name;
+            Money = money;
+            products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value; 
+            }
+        }
+
+        public int Money
+        {
+            get { return money; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                money = value;
+            }
+        }
+
+        public IReadOnlyCollection<Product> Products => products;
+
+        public void AddProduct(Product product)
+        {
+            Money -= product.Cost;
+            products.Add(product);
+        }
+    }
+}
