b10a85d30295c5ccc516fec03ff8f5060b95d210
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-08 10:37:22 +0200

    Add files via upload

---
+++ PolymorphismLab/Shapes/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double width, double height)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set { width = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return width*height; 
+        }
+
+       public override double CalculatePerimeter()
+       {
+            return 2*(width+height); 
+       }
+
+        public override string Draw()
+        {
+            return "Rectangle";
+        }
+    }
+}
