32502515c9f17ff2f53e022077ebfc5ae4588141
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-08 10:36:34 +0200

    Add files via upload

---
+++ PolymorphismLab/Animals/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string FavouriteFood
+        {
+            get { return favouriteFood; }
+            set { favouriteFood = value; }
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return "";
+        }
+
+    }
+}
