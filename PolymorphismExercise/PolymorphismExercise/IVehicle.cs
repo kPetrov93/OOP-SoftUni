c54e7ab83428cd495225e0014a20f6b5b2c2b496
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-09 10:49:01 +0200

    Add files via upload

---
+++ PolymorphismExercise/PolymorphismExercise/IVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismExercise
+{
+    public interface IVehicle
+    {
+        public double FuelQuantity { get; set; }
+
+        public  double FuelConsumption { get; set; }
+
+        void Drive(double km);
+
+        void Refuel(double liters);
+    }
+}
