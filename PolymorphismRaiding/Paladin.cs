8234a2aa68b7d062cf4f1958f2e8d8a09000a7ef
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-10 10:47:43 +0200

    Add files via upload

---
+++ PolymorphismRaiding/Paladin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismRaiding
+{
+    public class Paladin : BaseHero
+    {
+
+        public Paladin(string name, int power) : base(name, power)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {Name} healed for {Power}";
+        }
+    }
+}
