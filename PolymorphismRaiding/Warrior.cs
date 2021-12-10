838cef47c657355773744f4756bbc59c3d98d0dd
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-10 10:51:15 +0200

    Add files via upload

---
+++ PolymorphismRaiding/Warrior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismRaiding
+{
+    public class Warrior : BaseHero
+    {
+
+        public Warrior(string name, int power) : base(name, power)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {Name} hit for {Power} damage";
+        }
+    }
+}
