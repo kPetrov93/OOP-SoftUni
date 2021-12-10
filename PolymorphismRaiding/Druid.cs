e93a49586240c3dc4b02b8aa47dcba37af734647
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-10 10:47:03 +0200

    Add files via upload

---
+++ PolymorphismRaiding/Druid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismRaiding
+{
+    public class Druid : BaseHero
+    {
+
+        public Druid(string name,int power) : base(name,power)
+        {
+        }
+
+
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {Name} healed for {Power}";
+        }
+    }
+}
