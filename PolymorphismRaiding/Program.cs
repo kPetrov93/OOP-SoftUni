e1d79ebcad25584015dd56dd615cc7d4e2889824
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-10 10:51:03 +0200

    Add files via upload

---
+++ PolymorphismRaiding/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PolymorphismRaiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BaseHero> raiders = new List<BaseHero>();
+
+            int n = int.Parse(Console.ReadLine());
+            long total = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                string classType = Console.ReadLine();
+
+                BaseHero hero = null;
+
+                if (classType == "Druid")
+                {
+                    hero = new Druid(name,80);
+                }
+                else if (classType == "Paladin")
+                {
+                    hero = new Paladin(name,100);
+                }
+                else if (classType == "Rogue")
+                {
+                    hero = new Rogue(name,80);
+                }
+                else if (classType == "Warrior")
+                {
+                    hero = new Warrior(name,100);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid hero!");
+                    i -= 1;
+                    continue;
+                }
+
+                raiders.Add(hero);
+
+                total += hero.Power;
+            }
+
+            long bossPower = long.Parse(Console.ReadLine());
+
+            foreach (var item in raiders)
+            {
+                Console.WriteLine(item.CastAbility());
+            }
+
+            if (total >= bossPower)
+            {
+                Console.WriteLine("Victory!");
+            }
+            else
+            {
+                Console.WriteLine("Defeat...");
+            }
+        }
+    }
+}
---
+++ PolymorphismRaiding/Rogue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismRaiding
+{
+    public class Rogue : BaseHero
+    {
+
+        public Rogue(string name,int power) : base(name, power)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {Name} hit for {Power} damage";
+        }
+    }
+}
