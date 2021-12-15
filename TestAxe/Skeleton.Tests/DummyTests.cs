2c76997d765638f84684d07532285aa5df1b8675
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-15 12:10:28 +0200

    Add files via upload

---
+++ TestAxe/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLosesHealthWhenAttacked()
+        {
+            Axe axe = new Axe(5, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+            Assert.That(dummy.Health, Is.EqualTo(5), "Dummy healt points doesn't change.");
+        }
+
+        
+    }
+}
