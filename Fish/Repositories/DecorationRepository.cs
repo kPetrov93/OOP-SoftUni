cbec1848ca3a40b3d834ff6a4cdbd4adc13bb1dc
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-20 09:24:52 +0200

    Add files via upload

---
+++ Fish/Repositories/DecorationRepository.cs
@@ -0,0 +1,37 @@
+﻿using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Repositories
+{
+    public class DecorationRepository : IRepository<IDecoration>
+    {
+        private readonly List<IDecoration> decorations;
+
+        public DecorationRepository()
+        {
+            decorations = new List<IDecoration>();
+        }
+
+        public IReadOnlyCollection<IDecoration> Models => decorations;
+
+        public void Add(IDecoration model)
+        {
+            decorations.Add(model);
+        }
+
+        public IDecoration FindByType(string type)
+        {
+            return decorations.FirstOrDefault(x => x.GetType().Name == type);
+        }
+
+        public bool Remove(IDecoration model)
+        {
+            return decorations.Remove(model);
+        }
+    }
+}
